a01259c843b3752be1927bcf2e34ee934bd8011b
kruore <33648811+kruore@users.noreply.github.com>
2022-07-25 22:39:14 +0900

    2022_07_25
    Save - 백준 2750

---
+++ 20220725/Sort2750/Sort2750/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a = int.Parse(ReadLine());
+        int[] datas = new int[a];
+        for (int i = 0; i < a; i++)
+        {
+            datas[i] = int.Parse(ReadLine());
+        }
+
+        datas=Sort(datas);
+    }
+    static int[] Sort(int[] datas)
+    {
+        for (int i = 0; i < datas.Length; i++)
+        {
+            for (int j = i; j < datas.Length; j++)
+            {
+                int temp = 0;
+                if (datas[i] > datas[j])
+                {
+                    temp = datas[i];
+                    datas[i] = datas[j];
+                    datas[j] = temp;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+        foreach(var a in datas)
+        {
+            Console.WriteLine(a);
+        }
+        return datas;
+    }
+}
