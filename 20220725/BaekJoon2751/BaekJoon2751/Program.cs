b52c00d1271ba86d028d823dbac2744bffb68320
kruore <33648811+kruore@users.noreply.github.com>
2022-07-26 20:52:09 +0900

    0726
    BackJoon2751

---
+++ 20220725/BaekJoon2751/BaekJoon2751/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+class Programm
+{
+    private static LinkedList<int> A = new LinkedList<int>();
+    static void Main(string[] args)
+    {
+        var a = int.Parse(ReadLine());
+        var datas = new int[a];
+        for (int i = 0; i < a; i++)
+        {
+            A.AddLast(int.Parse(ReadLine()));
+        }
+
+    }
+
+    static void MergeSort(LinkedList<int> data, int left, int right)
+    {
+        if (data.Count > 0)
+        {
+            int mid = data.Count / 2 + 1;
+        }
+
+        if(left<right)
+        {
+            int mid = data.Count / 2;
+            MergeSort(data, left, mid);
+            MergeSort(data, mid+1, right);
+            Merge(data, left, mid, right);
+        }
+
+    }
+}
