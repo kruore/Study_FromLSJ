f6180c099c8c94331a33835a895a53c8801b7160
kruore <33648811+kruore@users.noreply.github.com>
2022-07-29 22:39:30 +0900

    20220729
    2108,1427

---
+++ 20220729/Backjooin1427/Backjooin1427/Backjooin1427.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int datas[10];
+
+int main()
+{
+	int data;
+	int b[10];
+	int count = 0;
+	cin >> data;
+	do {
+		b[count] = data % 10;
+		count++;
+		data=data / 10;
+	} while (data != 0);
+	for(int i =0; i<count;i++)
+	{
+		for (int j = i; j < count; j++)
+		{
+			if (b[i] < b[j])
+			{
+				int temp = b[j];
+				b[j] = b[i];
+				b[i] = temp;
+			}
+		}
+		cout << b[i];
+	}
+}
