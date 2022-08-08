efa39aa00265dec8d8252c8e82936eff8a49c8b9
kruore <33648811+kruore@users.noreply.github.com>
2022-08-08 23:50:45 +0900

    0808_BeckJoon1085

---
+++ 20220808/Baekjoon1764/Baekjoon1764/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+var a = Console.ReadLine().Split(' ');
+int N = int.Parse(a[0]);
+int M = int.Parse(a[1]);
+
+Dictionary<int, string> data = new Dictionary<int, string>();
+List<string> answer = new List<string>();
+
+
+var aString = Enumerable.Range(0, N).Select(_ => Console.ReadLine()).ToArray();
+var bString = Enumerable.Range(0, M).Select(_ => Console.ReadLine()).ToArray();
+Console.WriteLine(Enumerable.Intersect(aString, bString).OrderBy(x => x).Count());
+foreach(var ans in Enumerable.Intersect(aString,bString).OrderBy(x=>x))
+{
+    Console.WriteLine(ans);
+}
---
+++ 20220808/BeckJoon1085/BeckJoon1085/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+int[] x = new int[3];
+int[] y = new int[3];
+
+for(int i =0;i< 3;i++)
+{
+    string[] datas = Console.ReadLine().Split(' ');
+    x[i] = int.Parse(datas[0]);
+    y[i] = int.Parse(datas[1]);
+}
+int[] minx = x.GroupBy(x => x).Where(g => g.Count() == 1).Select(y => y.Key).ToArray();
+int[] miny = y.GroupBy(x => x).Where(g => g.Count() == 1).Select(y => y.Key).ToArray();
+
+Console.WriteLine($"{minx[0]} {miny[0]}");
