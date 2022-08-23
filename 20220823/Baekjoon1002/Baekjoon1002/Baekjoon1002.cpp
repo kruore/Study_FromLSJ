155f2dd720ac1eb23d383930bde955babe988f50
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-08-23 23:40:36 +0900

    0823

---
+++ 20220823/Baekjoon1002/Baekjoon1002/Baekjoon1002.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int x1, y1, z1, x2, y2, z2;
+    int testcase;
+    int answer;
+    int circle00, circle01;
+    cin >> testcase;
+    for (int i = 0; i < testcase; i++)
+    {
+        cin >> x1 >> y1 >> z1 >> x2 >> y2 >> z2;
+        answer = ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2));
+        circle00 = (z1 - z2) * (z1 - z2);
+        circle01 = (z1 + z2) * (z1 + z2);
+        if (answer == 0)
+        {
+            if (circle00 == 0)
+            {
+                cout << "-1" << "\n";
+            }
+            else
+            {
+                cout << "0" << "\n";
+            }
+        }
+        else if (answer == circle00 || answer == circle01)
+        {
+            cout << "1" << '\n';
+        }
+        else if (answer > circle00 && answer < circle01)
+        {
+            cout << "2" << '\n';
+        }
+        else
+        {
+            cout << "0" << "\n";
+        }
+    }
+
+}
---
+++ 20220823/baekjnoon1004/baekjnoon1004/baekjnoon1004.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, x1, x2, y1, y2, m;
+	int count;
+	int z1, z2, r;
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		count = 0;
+		cin >> x1 >> y1 >> x2 >> y2 >> m;
+
+		for (int j = 0; j < m; j++)
+		{
+			cin >> z1 >> z2 >> r;
+			int distanceA = (z1 - x1) * (z1 - x1) + (z2 - y1) * (z2 - y1);
+			int distanceB = (z1 - x2) * (z1 - x2) + (z2 - y2) * (z2 - y2);
+			int length = r * r;
+			bool a, b;
+			a = distanceA > length ? false : true;
+			b = distanceB > length ? false : true;
+			if (a != b)
+			{
+				count++;
+			}
+		}
+		cout << count << "\n";
+	}
+}
