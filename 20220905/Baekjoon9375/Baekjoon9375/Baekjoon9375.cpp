4aa19c36e49eabd46547c149b4a73c02647aca86
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-09-05 23:43:36 +0900

    20220905
    다리놓기랑 옷

---
+++ 20220905/Baekjoon9375/Baekjoon9375/Baekjoon9375.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+	int length = 0;
+	cin >> length;
+	for (int i = 0; i < length; i++)
+	{
+		map<string, int> myMap;
+		int wearCout;
+		cin >> wearCout;
+		for (int j = 0; j < wearCout; j++)
+		{
+			string cloth, wear;
+			cin >> cloth >> wear;
+			if (myMap.count(wear) == 0)
+			{
+				myMap.insert({ wear, 1 });
+			}
+			else
+			{
+				myMap[wear]++;
+			}
+		}
+		int answer = 1;
+		for (auto x : myMap)
+		{
+			answer *= x.second + 1;
+		}
+		answer -= 1;
+		cout << answer << "\n";
+	}
+}
