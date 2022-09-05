0ea5e6a90d0a3b3b6836dfb17940734d649a87b5
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-09-05 22:40:27 +0900

    20220905

---
+++ 20220905/baekjoon1010/baekjoon1010/baekjoon1010.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int remember[31][31];
+
+int main()
+{	
+	int length, answer = 0;
+	cin >> length;
+	int left, right;
+	for (int i = 0; i <= 30; i++)
+	{
+		remember[1][i] = i;
+	}
+	for (int i = 2; i < 30; i++)
+	{
+		for (int j = i; j < 30; j++)
+		{
+			for (int k = j - 1; k >= 1; k--)
+			{
+				remember[i][j] += remember[i - 1][k];
+			}
+		}
+	}
+
+	for (int i = 0; i < length; i++)
+	{
+		cin >> left >> right;
+		cout << remember[left][right]<<"\n";
+	}
+}
