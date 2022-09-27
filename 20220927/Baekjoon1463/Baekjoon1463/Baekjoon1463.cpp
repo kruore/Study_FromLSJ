2b85987c8ecc4d4c1fb022fb9e845db045b472e1
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-09-27 23:50:19 +0900

    11
    2solve

---
+++ 20220927/Baekjoon1463/Baekjoon1463/Baekjoon1463.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int dp[1000001];
+int number;
+void Solution(int numbers);
+int main()
+{
+	cin >> number;
+	Solution(number);
+}
+
+void Solution(int numbers)
+{
+	dp[0]= 0;
+	dp[1] = 0;
+	dp[2] = 1;
+	dp[3] = 1;
+	for (int i = 4; i <= numbers; i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0)
+		{
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		}
+		if (i % 3 == 0)
+		{
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+		}
+	}
+	cout << dp[numbers];
+}
---
+++ 20220927/Baekjoon2579/Baekjoon2579/Baekjoon2579.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+void Stepper();
+int _DP[301];
+int _temp[301];
+int _length;
+int main()
+{
+    cin >> _length;
+    for (int i = 1; i <= _length; i++)
+    {
+        cin >> _DP[i];
+    }
+    Stepper();
+}
+
+void Stepper()
+{
+    _temp[1] = _DP[1];
+    _temp[2] = _DP[1]+_DP[2];
+    _temp[3] = max(_DP[1] + _DP[3],_DP[2]+_DP[3]);
+
+    for (int i = 4; i <= _length; i++)
+    {
+        _temp[i] = max(_temp[i - 2] + _DP[i], _temp[i - 3] + _DP[i - 1] + _DP[i]);
+    }
+    cout << _temp[_length];
+}
