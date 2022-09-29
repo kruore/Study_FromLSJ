de44af50c8c6fe43aea9f71408314e505fd36a15
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-09-29 23:08:49 +0900

    0929
    Save

---
+++ 20220929/Baekjoon10844/Baekjoon10844/Baekjoon10844.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#define MAXIMUM 1000000000
+
+using namespace std;
+//45654 = 계단 수
+
+int answer;
+int _length;
+int dp[10] = { 0 , };
+int _numberSize;
+void Solution();
+int main()
+{
+	cin >> _length;
+	Solution();
+}
+void Solution()
+{
+	/// 0~ 9까지의 각 마지막 자리의? 0~9까지의 개수를 새면 된다는거잖아?
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 1;
+	dp[3] = 1;
+	dp[4] = 1;
+	dp[5] = 1;
+	dp[6] = 1;
+	dp[7] = 1;
+	dp[8] = 1;
+	dp[9] = 1;
+
+	for (int i = 1; i < _length; i++)
+	{
+		int temp[10];
+		temp[0] = (dp[1] % MAXIMUM);
+		temp[1] = (dp[0] + dp[2]) % MAXIMUM;
+		temp[2] = (dp[1] + dp[3]) % MAXIMUM;
+		temp[3] = (dp[2] + dp[4]) % MAXIMUM;
+		temp[4] = (dp[3] + dp[5]) % MAXIMUM;
+		temp[5] = (dp[4] + dp[6]) % MAXIMUM;
+		temp[6] = (dp[5] + dp[7]) % MAXIMUM;
+		temp[7] = (dp[6] + dp[8]) % MAXIMUM;
+		temp[8] = (dp[7] + dp[9]) % MAXIMUM;
+		temp[9] = dp[8]% MAXIMUM;
+		copy(temp,temp+10,dp);
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		answer += dp[i];
+		answer=answer% MAXIMUM;
+	}
+
+	cout << answer;
+}
+
---
+++ 20220929/Baekjoon2156/Baekjoon2156/Baekjoon2156.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int wine[10001];
+int dp[10001];
+int length;
+void Solution();
+int main()
+{
+	cin >> length;
+	for (int i = 1; i <= length; i++)
+	{
+		cin >> wine[i];
+	}
+	Solution();
+}
+
+void Solution()
+{
+	dp[1] = wine[1];
+	dp[2] = dp[1] + wine[2];
+	for (int i = 3; i <= length; i++)
+	{
+		dp[i] = max(dp[i - 3] + wine[i - 1] + wine[i], dp[i - 2] + wine[i]);
+		dp[i] = max(dp[i - 1], dp[i]);
+	}
+	cout << dp[length];
+}
