bade3fdf6af721d71e02e0dd18f47d2be3280991
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-10-11 20:47:41 +0900

    20221011
    Baekjoon9251

---
+++ 20221006/Baekjoon12865/Baekjoon12865/소스.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int max_weight;
+int max_value;
+pair<int, int> scs;
+vector<pair<int, int>> data;
+
+int main()
+{
+	cin >> max_weight >> max_value;
+	for (int i = 0; i < max_weight; i++)
+	{
+		cin >> scs.first >> scs.second;
+
+	}
+}
