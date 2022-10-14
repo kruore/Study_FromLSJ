b057a752a5eb3eae847da5a31204306901519641
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-10-14 23:53:05 +0900

    20221014
    Data Saved

---
+++ 20221014/Baekjoon/Baekjoon/Baekjoon.cpp
@@ -0,0 +1,33 @@
+﻿#define collection cin.tie(NULL); ios_base::sync_with_stdio(false);
+#include <iostream>
+#include <vector>
+
+int a[11];
+using namespace std;
+void Solution();
+void Init();
+int main()
+{
+	Init();
+	Solution();
+}
+void Solution()
+{
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	int sum = 0;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		sum += m / a[i];
+		m = m % a[i];
+	}
+	cout << sum << "\n";
+}
+void Init()
+{
+	collection;
+}
---
+++ 20221014/baekjoon1931/baekjoon1931/baekjoon1931.cpp
@@ -0,0 +1,43 @@
+﻿#define collection cin.tie(NULL); ios_base::sync_with_stdio(false);
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// job 스케쥴러라고 볼 수 있다.?
+using namespace std;
+void Solution();
+void Init();
+int length;
+vector<pair<int, int>> meetingRoom;
+int main()
+{
+	Init();
+	Solution();
+}
+void Solution()
+{
+	sort(meetingRoom.begin(), meetingRoom.end());
+	int time = meetingRoom[0].first;
+	int count = 1;
+	for (int i = 1; i < length; i++)
+	{
+		if (time <= meetingRoom[i].second)
+		{
+			count++;
+			time = meetingRoom[i].first;
+		}
+	}
+	cout << count;
+
+}
+void Init()
+{
+	collection;
+	cin >> length;
+	for (int i = 0; i < length; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		meetingRoom.push_back(make_pair(b, a));
+	}
+}
