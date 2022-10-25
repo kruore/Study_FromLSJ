3a7afe95f7f543ad99e3fba71fdbe0fcffe1d444
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-10-25 22:52:56 +0900

    20221025
    backjoon1966

--- 20221025/Baekjoon1966/Baekjoon1966/Baekjoon1966.cpp
+++ 20221025/Baekjoon1966/Baekjoon1966/Baekjoon1966.cpp
@@ -1,20 +1,60 @@
-﻿// Baekjoon1966.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
+﻿#define collection cin.tie(NULL); ios_base::sync_with_stdio(false);
 #include <iostream>
-
+#include <queue>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n, m;
+void Solution();
+void Init();
 int main()
 {
-    std::cout << "Hello World!\n";
+	Init();
+	Solution();
 }
+void Solution()
+{
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+}
+void Init()
+{
+	collection;
+	int num = 0;
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		queue<pair<int, int>> q;
+		priority_queue<int> maxImp;
+		cin >> n;
+		cin >> m;
+		for (int j = 0; j < n; j++)
+		{
+			int data;
+			cin >> data;
+			q.push(make_pair(j, data));
+			maxImp.push(data);
+		}
+		int printcount = 0;
+		while (!maxImp.empty())
+		{
+			int index = q.front().first;
+			int value = q.front().second;
+	
+			q.pop();
+			if (maxImp.top() == value)
+			{
+				maxImp.pop();
+				printcount++;
+				if (index == m)
+				{
+					cout << printcount<<"\n";
+					break;
+				}
+			}
+			else
+			{
+				q.push(make_pair(index, value));
+			}
+		}
+	}
+}
