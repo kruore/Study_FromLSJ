72929716c43b7e37b6106d17dff62f399d0ee4fe
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-10-26 23:34:19 +0900

    20221026
    Complete Queue

---
+++ 20221025/baekjoon5430/baekjoon5430/baekjoon5430.cpp
@@ -0,0 +1,100 @@
+﻿#define collection cin.tie(0); cout.tie(0),ios_base::sync_with_stdio(false);
+#include <iostream>
+#include <deque>
+#include <string>
+
+using namespace std;
+int T, length;
+string _data;
+void Solution();
+string _func;
+deque<int> _realData;
+void Init();
+int main()
+{
+	Init();
+}
+void Init()
+{
+	collection;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		//수행 함수
+		cin >> _func;
+		//배열에 들어있는 수 개수
+		cin >> length;
+		cin >> _data;
+		bool isError = false;
+		bool _reverse = true;
+
+		_data = _data.substr(1, _data.size());
+		string dart = "";
+		for (int j = 0; j < _data.size(); j++)
+		{
+			
+			if ('0' <= _data[j] && _data[j] <= '9')
+			{
+				dart += _data[j];
+			}
+			else if (_data[j] == ',' || _data[j] == ']' && dart != "")
+			{
+				int insert = atoi(dart.c_str());
+				_realData.push_back(atoi(dart.c_str()));
+				dart = "";
+			}
+		}
+		for (int j = 0; j < _func.size(); j++)
+		{
+			if (_func[j] == 'R')
+			{
+				_reverse = !_reverse;
+			}
+			else if (_func[j] == 'D')
+			{
+				if (_realData.empty())
+				{
+					cout << "error" << "\n";
+					isError = true;
+					break;
+				}
+				else
+				{
+					if (_reverse)
+					{
+						_realData.pop_front();
+					}
+					else
+					{
+						_realData.pop_back();
+					}
+				}
+			}
+		}
+		if (!isError)
+		{
+			string answer = "[";
+			int _realdataSize = _realData.size();
+			for (int z = 0; z < _realdataSize; z++)
+			{
+				if (_reverse)
+				{
+					answer += to_string(_realData.front());
+					_realData.pop_front();
+				}
+				else
+				{
+					answer += to_string(_realData.back());
+					_realData.pop_back();
+				}
+				if (!_realData.empty())
+				{
+					answer += ",";
+				}
+			}
+			answer += "]";
+			cout << answer << "\n";
+		}
+	}
+
+}
