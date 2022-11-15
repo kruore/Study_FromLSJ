8116c83dcdc0d14e0f308893203b82de529a3335
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-11-15 23:12:40 +0900

    20221115

---
+++ 20221114/baekjoon6549/baekjoon6549/baekjoon6549.cpp
@@ -0,0 +1,40 @@
+﻿#define collection cin.tie(NULL); ios_base::sync_with_stdio(false);
+#define MAX 100001; 
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+int _length = MAX;
+int manu[100001] = {0,};
+int answer = 108;
+void Solution();
+void Init();
+stack<int> slot;
+int main()
+{
+	Init();
+}
+
+void Init()
+{
+	collection;
+	while (true)
+	{
+		cin >> _length;
+		if (_length == 0)
+		{
+			break;
+		}
+		for (int i = 0; i < _length; i++)
+		{
+			cin >> manu[i];
+		}
+		Solution();
+	}
+}
+void Solution()
+{
+	
+}
---
+++ 20221114/segmenttree/segmenttree/segmenttree.cpp
@@ -0,0 +1,26 @@
+﻿#include <math.h>
+#include <iostream>
+int* tree;
+int A[10] = { 1,4,2,3,5,1,2,4,5,3 };
+using namespace std;
+long long init(int index, int start, int end)
+{
+    if (start == end)
+        tree[index] = A[start];
+    else
+        tree[index] = init(index * 2 + 1, start, (start + end) / 2) + init(index * 2 + 2, (start + end) / 2 + 1, end);
+
+    return tree[index];
+}
+void main()
+{
+    int N = 0;
+     cin>> N;
+    int h = ceil(log2(N));
+    int tree_size = 1 << (h + 1);
+
+    tree = new int[tree_size];
+    init(0, 0, N - 1);
+
+    delete[] tree;
+}
