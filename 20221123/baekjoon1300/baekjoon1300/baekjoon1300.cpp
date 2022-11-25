4277e39b15205fdbc4faa501c7d6f37853fdd656
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-11-25 23:35:56 +0900

    20221125
    commit

---
+++ 20221123/baekjoon1300/baekjoon1300/baekjoon1300.cpp
@@ -0,0 +1,49 @@
+﻿#define collection cin.tie(NULL); ios_base::sync_with_stdio(false);
+#include <iostream>
+#include <vector>
+
+typedef long long ll;
+using namespace std;
+ll _length, k;
+void Solution();
+void Init();
+int main()
+{
+	Init();
+	Solution();
+}
+void Solution()
+{
+
+}
+void Init()
+{
+	collection;
+	cin >> _length>>k;
+
+	k = min((ll)1000000000, k);
+
+	ll Low = 1;
+	ll High = _length * _length;
+	ll count;
+
+	while (Low <= High)
+	{
+		ll Mid = (Low + High) / 2;
+		ll sum = 0;
+		for (ll i = 1; i <= _length; i++)
+		{
+			sum += min(Mid / i, _length);
+		}
+		count = sum;
+		if (count >= k)
+		{
+			High = Mid - 1;
+		}
+		else
+		{
+			Low = Mid + 1;
+		}
+	}
+	cout << Low;
+}
