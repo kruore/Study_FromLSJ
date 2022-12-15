f91381058aa406c734953ee0fa7c02a6f6f328c4
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-12-15 23:39:53 +0900

    1215

---
+++ 20221208/10872Backjoon/10872Backjoon/10872Backjoon.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int repeat = 0;
+int answer = 1;
+int Factorial(int data)
+{
+	if (data >2)
+	{
+		data *= Factorial(data - 1);
+		return data;
+	}
+}
+
+int main()
+{
+	cin >> repeat;
+	int data = Factorial(repeat);
+	cout << data;
+}
+
