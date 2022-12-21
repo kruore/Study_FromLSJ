2ddf76d1a2e5cdee46a2c4869e19eabed2fd8d19
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-12-21 23:22:00 +0900

    1221
    Baekjoon DFS+ one dimension 배열 전환

--- 20221208/Baekjoon1520/Baekjoon1520/Baekjoon1520.cpp
+++ 20221208/Baekjoon1520/Baekjoon1520/Baekjoon1520.cpp
@@ -3,9 +3,14 @@
 #include <vector>
 
 using namespace std;
-int _length;
+int M, N;
 void Solution();
 void Init();
+int numbers[501][501];
+int DP[501][501] = { 0, };
+int xl[4] = { -1,0,1,0 };
+int yl[4] = { 0,-1,0,1 };
+int DFS(int y, int x);
 int main()
 {
 	Init();
@@ -13,9 +18,35 @@ int main()
 void Init()
 {
 	collection;
-	cin >> _length;
-	for (int i = 0; i < _length; i++)
+	cin >> M >> N;
+	for (int i = 1; i <= M; i++)
 	{
-
+		for (int j = 1; j <= N; j++)
+		{
+			cin >> numbers[i][j];
+			DP[i][j] = -1;
+		}
 	}
+	cout << DFS(M, N);
+	//세로줄 비교
+}
+int DFS(int x, int y)
+{
+    if (x == 1 && y == 1) return 1;
+
+    if (DP[x][y] == -1) {
+        DP[x][y] = 0;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = xl[i] + x;
+            int ny = yl[i] + y;
+
+            if (nx >= 1 && nx <= M && ny >= 1 && ny <= N) {
+                if (numbers[x][y] < numbers[nx][ny])
+                    DP[x][y] += DFS(nx, ny);
+            }
+        }
+    }
+
+    return DP[x][y];
 }
