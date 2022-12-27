a6b139c95d68eb969c8c91bdede379b898b7b81e
Lee Sang Jun <33648811+kruore@users.noreply.github.com>
2022-12-27 21:41:32 +0900

    20221227

---
+++ 20221208/baekjoon2293/baekjoon2293/baekjoon2293.cpp
@@ -0,0 +1,31 @@
+﻿#define collection cin.tie(NULL); ios_base::sync_with_stdio(false);
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int N,K;
+int coins[10000];
+int DP[10001];
+void Init();
+int main()
+{
+	Init();
+}
+void Init()
+{
+	collection;
+	cin >> N >> K;
+	DP[0] = 1;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> coins[i];
+	}
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = coins[i]; j <= K; j++)
+		{
+			DP[j] += DP[j - coins[i]];
+		}
+	}
+	cout << DP[K] << "\n";
+}
